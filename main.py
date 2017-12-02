24c8de71046cb1394ef5d8f5b8fd053f1a0f0c10
mpralat <pralatmarta@gmail.com>
2017-12-02 16:48:36 +0100

    Add configurable serial number

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import sys
-import serial
 import argparse
-from datetime import datetime
+import re
+import sys
+
 from websocket import run as websocket_run
 
 SERVER_URL = 'wss://co-sensor.herokuapp.com'
+
+
+def hex_type(serial_no, pattern=re.compile(r'^[0-9A-F]{16}$')):
+    if not pattern.match(serial_no):
+        raise argparse.ArgumentTypeError('Serial number should be a hex number, 16 characters long')
+    return serial_no
 
 
 def main():
@@ -13,13 +19,16 @@ def main():
         '--test', dest="test", action="store_true", default=False
     )
     parser.add_argument('url', nargs='?', default=SERVER_URL)
+    parser.add_argument('--serial', '-s', default="E8A44117E02E4147", type=hex_type)
 
     args = parser.parse_args(sys.argv[1:])
     if args.test:
         from test_sensor import TestSensor as Sensor
+        sensor = Sensor(args.url, args.serial)
     else:
         from sensor import Sensor as Sensor
-    sensor = Sensor(args.url)
+        sensor = Sensor(args.url)
+
 
     websocket_run(sensor)
 
--- test_sensor.py
+++ test_sensor.py
@@ -5,12 +5,14 @@ import time
 
 class TestSensor(SensorInterface):
 
-    def __init__(self, url):
+    def __init__(self, url, serial_no):
         self.start_measure = 5
+        self._serial_no = serial_no
+        print('SERIAL NUMBER: {}'.format(self._serial_no))
         super().__init__(url)
 
     def get_serial_number(self):
-        return "E8A44117E02E4147"
+        return self._serial_no
 
     def measure(self):
         time.sleep(1.0 + random.random() * 0.2)
